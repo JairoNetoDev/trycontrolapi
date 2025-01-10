b74ec058f1379457f16114b2d2701ac9308dabd5
Jairo Neto <jairo.neto.dev@gmail.com>
2025-01-10 19:31:46 -0300

    adding base project

---
+++ src/main/java/com/trycontrol/TrycontrolApplication.java
@@ -0,0 +1,13 @@
+package com.trycontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrycontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrycontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trycontrol/TrycontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trycontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrycontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
